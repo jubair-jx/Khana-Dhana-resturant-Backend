69630ad71568dbb70b6972eecc81a4799bf61516
jubair-jx <jubair.official97@gmail.com>
2023-06-04 11:56:36 +0600

    Delete Api complete

--- index.js
+++ index.js
@@ -53,8 +53,9 @@ async function run() {
       res.send(result);
     });
     //delete cart api
-    app.get("/cart/:id", async (req, res) => {
+    app.delete("/carts/:id", async (req, res) => {
       const id = req.params.id;
+
       const query = { _id: new ObjectId(id) };
       const result = await cartCollection.deleteOne(query);
       res.send(result);
